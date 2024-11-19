871f6f2c851ad370920ff7e152c66ede05c9df56
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2024-11-19 12:18:08 +0530

    Update socket.js

--- server/socket.js
+++ server/socket.js
@@ -4,7 +4,7 @@ import Channel from "./models/channelModel.js";
 const setupSocket = (server) => {
   const io = new SocketIoServer(server, {
     cors: {
-      origin:"http://localhost:5173",
+      origin:"http://mohammedsuhail364.github.io/Chat-Us",
       methods: ["GET", "POST"],
       credentials: true,
     },
